90011bbb8a0cd486741f6af867c7d80210ca595b
Edward Capriolo <edlinuxguru@gmail.com>
2025-02-17 16:42:36 -0500

    And a queue is born

--- src/main/java/io/teknek/collections/queue/ByteBufferIntQueue.java
+++ src/main/java/io/teknek/collections/queue/ByteBufferIntQueue.java
@@ -1,27 +1,87 @@
 package io.teknek.collections.queue;
 
+import io.teknek.collections.evolving.Maybe;
+
 import java.nio.ByteBuffer;
+import java.time.Duration;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class ByteBufferIntQueue {
 
-        private ByteBuffer bb;
-        int tail;
-        int head;
+    final ReentrantLock lock = new ReentrantLock();
+    final Condition notFull  = lock.newCondition();
+    final Condition notEmpty = lock.newCondition();
 
-        public ByteBufferIntQueue(int capacity){
-            bb = ByteBuffer.allocate(capacity * 4);
+    private final int capacity;
+    //https://stackoverflow.com/questions/43069910/volatile-and-arrayblockingqueue-and-perhaps-other-concurrent-objects
+    private final ByteBuffer underlyingBuffer;
+    private int tail;
+    private int head;
+
+    public ByteBufferIntQueue(int capacity) {
+        this.capacity = capacity;
+        underlyingBuffer = ByteBuffer.allocate(capacity * 4);
+    }
+
+    public void enqueue(int i) throws InterruptedException {
+        final ReentrantLock lock = this.lock;
+        lock.lockInterruptibly();
+        try {
+            while(tail == capacity){
+                notFull.await();
+            }
+            underlyingBuffer.putInt(tail * 4, i);
+            tail = (tail + 1) % underlyingBuffer.capacity();
+            notEmpty.signal();
+        } finally{
+            lock.unlock();
         }
+    }
 
-        public void enque(int i){
-            //check that the queue is not full
-            bb.putInt(tail* 4, i);
-            tail = (tail + 1);
+    public Maybe<Integer> dequeue(Duration toWait) throws InterruptedException {
+        long nanos = toWait.toNanos();
+        final ReentrantLock lock = this.lock;
+        lock.lockInterruptibly();
+        try {
+            while (head == tail){
+                if (nanos <= 0L){
+                    return Maybe.nothing();
+                }
+                nanos = notEmpty.awaitNanos(nanos);
+            }
+            return Maybe.possibly(underlyingDequeueAndSignal());
+        } finally {
+            lock.unlock();
         }
+    }
 
-        public int dequeue(){
-            int b = bb.getInt(head *  4);
-            head = (head + 1) ;
-            return b;
+    public int dequeue() throws InterruptedException {
+        final ReentrantLock lock = this.lock;
+        lock.lockInterruptibly();
+        try {
+            while (head == tail){
+                notEmpty.await();
+            }
+            return underlyingDequeueAndSignal();
+        } finally {
+            lock.unlock();
         }
+    }
 
+    private int underlyingDequeueAndSignal(){
+        int b = underlyingBuffer.getInt(head * 4);
+        head = (head + 1) % underlyingBuffer.capacity();
+        notFull.signal();
+        return b;
+    }
+
+    @Override
+    public String toString() {
+        return "ByteBufferIntQueue{" +
+                "bb=" + underlyingBuffer.capacity() +
+                ", tail=" + tail +
+                ", head=" + head +
+                '}';
+    }
 }
--- src/test/java/io/teknek/collection/queue/ByteBufferIntQueueTest.java
+++ src/test/java/io/teknek/collection/queue/ByteBufferIntQueueTest.java
@@ -1,17 +1,24 @@
 package io.teknek.collection.queue;
 
+import io.teknek.collections.evolving.Maybe;
 import io.teknek.collections.queue.ByteBufferIntQueue;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ByteBufferIntQueueTest {
 
     @Test
-    public void test(){
+    public void test() throws InterruptedException {
         ByteBufferIntQueue queue = new ByteBufferIntQueue(10);
-        queue.enque(5);
-        queue.enque(6);
-        Assertions.assertEquals(5, queue.dequeue());
-        Assertions.assertEquals(6, queue.dequeue());
+        queue.enqueue(5);
+        queue.enqueue(6);
+        assertEquals(5, queue.dequeue());
+        assertEquals(6, queue.dequeue());
+        assertEquals(Maybe.nothing(), queue.dequeue(Duration.ofMillis(1)));
+        //This will block forever without a second thread
+        //IllegalArgumentException expected = Assertions.assertThrows(IllegalArgumentException.class, queue::dequeue);
     }
 }
